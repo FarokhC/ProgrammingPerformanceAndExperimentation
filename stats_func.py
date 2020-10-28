9d79e351673fc248c6de110aa312d82d857288f1
Khang Doan <khang.doan@sjsu.edu>
2020-10-27 20:41:19 -0700

    Added python module for statisical calculation

--- stats_func.py
+++ stats_func.py
@@ -3,7 +3,10 @@ from functools import reduce
 
 
 def getAvg(input_list: list) -> float:
-    return reduce((lambda x, y: x + y), [float(x) for x in input_list]) / len(input_list)
+    return reduce(
+        (lambda x, y: x + y),
+        [float(x) for x in input_list]
+    ) / len(input_list)
 
 
 def getSTD(input_list: list) -> float:
